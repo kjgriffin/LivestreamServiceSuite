c56045cb0d4b670773543ae85775eafd52b0c89a
kristopher.j.griffin <kristopher.j.griffin@gmail.com>
2020-09-01 12:15:04 -0400

    debug block paragraph renderer

--- Xenon/LayoutEngine/BlockParagraphLayoutEngine.cs
+++ Xenon/LayoutEngine/BlockParagraphLayoutEngine.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,9 @@ namespace Xenon.LayoutEngine
     class BlockParagraphLayoutEngine
     {
 
-        static public List<(List<string> words, float width, float height, bool linestart, bool fulltextonline)> LayoutParagraph(Rectangle textblock, Font font, List<string> words)
+        static public List<BlockParagraph> LayoutParagraph(Rectangle textblock, Font font, List<string> words)
         {
-            List<(List<string>, float, float, bool, bool)> res = new List<(List<string>, float, float, bool, bool)>();
+            List<BlockParagraph> res = new List<BlockParagraph>();
             Bitmap layoutbmp = new Bitmap(textblock.Width, textblock.Height);
             Graphics gfx = Graphics.FromImage(layoutbmp);
             StringFormat sf = new StringFormat() { LineAlignment = StringAlignment.Center, Alignment = StringAlignment.Near };
@@ -48,8 +49,71 @@ namespace Xenon.LayoutEngine
                 startwords += wordcount - 1;
             }
 
+            Debug.WriteLine($"foramtting\n{string.Join(System.Environment.NewLine, words)}\n[produced]");
+            foreach (var b in res)
+            {
+                Debug.WriteLine(b);
+            }
+
             return res;
 
+        }
+    }
+
+    public struct BlockParagraph
+    {
+        public List<string> words;
+        public float width;
+        public float height;
+        public bool linestart;
+        public bool fulltextonline;
+
+        public BlockParagraph(List<string> words, float width, float height, bool linestart, bool fulltextonline)
+        {
+            this.words = words;
+            this.width = width;
+            this.height = height;
+            this.linestart = linestart;
+            this.fulltextonline = fulltextonline;
+        }
+
+        public override string ToString()
+        {
+            return $"[block paragraph] <{string.Join(' ', words)}>";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is BlockParagraph other &&
+                   EqualityComparer<List<string>>.Default.Equals(words, other.words) &&
+                   width == other.width &&
+                   height == other.height &&
+                   linestart == other.linestart &&
+                   fulltextonline == other.fulltextonline;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(words, width, height, linestart, fulltextonline);
+        }
+
+        public void Deconstruct(out List<string> words, out float width, out float height, out bool linestart, out bool fulltextonline)
+        {
+            words = this.words;
+            width = this.width;
+            height = this.height;
+            linestart = this.linestart;
+            fulltextonline = this.fulltextonline;
+        }
+
+        public static implicit operator (List<string> words, float width, float height, bool linestart, bool fulltextonline)(BlockParagraph value)
+        {
+            return (value.words, value.width, value.height, value.linestart, value.fulltextonline);
+        }
+
+        public static implicit operator BlockParagraph((List<string> words, float width, float height, bool linestart, bool fulltextonline) value)
+        {
+            return new BlockParagraph(value.words, value.width, value.height, value.linestart, value.fulltextonline);
         }
     }
 }
