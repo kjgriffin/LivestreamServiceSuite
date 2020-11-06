26937b52c5af02764b9da27d0daa3e120c9ca96b
kristopher.j.griffin <kristopher.j.griffin@gmail.com>
2020-11-06 16:48:47 -0500

    begin work on new slides to support multiple slide windows

---
+++ Slide Presentation/Slide.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slide_Presentation
+{
+    public interface ISlide
+    {
+        string Filename { get; set; }
+        Uri SourceUri { get; }
+    }
+}
---
+++ Slide Presentation/SlideDeck.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Slide_Presentation
+{
+    public interface ISlideDeck
+    {
+        List<ISlide> Slides { get; set; }
+    }
+}
