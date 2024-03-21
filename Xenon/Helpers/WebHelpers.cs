31ac681fabb09d4127fa0f235ac83bb3c4a4dfda
kjgriffin <kristopher.j.griffin@gmail.com>
2024-03-21 10:38:07 -0400

    add some polly http client correctness

--- Xenon/Helpers/WebHelpers.cs
+++ Xenon/Helpers/WebHelpers.cs
@@ -1,11 +1,23 @@
-﻿using System.Net.Http;
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using Polly;
+using Polly.Extensions.Http;
+
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Xenon.Helpers
 {
     public class WebHelpers
     {
-        public static readonly HttpClient httpClient = new HttpClient();
+        public static HttpClient httpClient
+        {
+            get
+            {
+                return XenonHttpClientBuilder.GetFactory().CreateClient();
+            }
+        }
 
         public static async Task<string> DownloadText(string url)
         {
@@ -13,6 +25,40 @@ namespace Xenon.Helpers
             var text = await resp.Content.ReadAsStringAsync();
 
             return text;
+        }
+
+    }
+
+    static class XenonHttpClientBuilder
+    {
+        public const string XENON_HTTP_CLIENT = "XenonClientBuilder";
+        internal static IServiceProvider _services { get; private set; }
+
+        internal static IHttpClientFactory GetFactory()
+        {
+            if (_services == null)
+            {
+                ConfigureAndBuild();
+            }
+            var factory = _services.GetService<IHttpClientFactory>();
+            return factory;
+        }
+
+        internal static void ConfigureAndBuild()
+        {
+            ServiceCollection services = new ServiceCollection();
+            services.AddHttpClient(XENON_HTTP_CLIENT)
+                    .SetHandlerLifetime(TimeSpan.FromMinutes(5))
+                    .AddPolicyHandler(GetRetryPolicy());
+
+            _services = services.BuildServiceProvider();
+        }
+
+        private static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions.HandleTransientHttpError()
+                                       .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                                       .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt) / 2));
         }
 
     }
